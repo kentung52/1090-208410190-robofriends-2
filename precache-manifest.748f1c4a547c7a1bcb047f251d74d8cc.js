167ac75f57f3fc4c1b4757acba440ff2ca6ceb86
208410190 <208410190@gms.tku.edu.tw>
2020-10-14 21:53:49 +0800

    Updates

---
+++ precache-manifest.748f1c4a547c7a1bcb047f251d74d8cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa34d5f31fd7caa9e5079beea2f76cb",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9223dd751ddab516f23b",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/css/main.28accb84.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9223dd751ddab516f23b",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/main.00d95962.chunk.js"
+  },
+  {
+    "revision": "2164df3529fadda80a9a",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/runtime-main.4b3fa047.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//kentung52.github.io/1091-208410190-robofriends/precache-manifest.748f1c4a547c7a1bcb047f251d74d8cc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//kentung52.github.io/1091-208410190-robofriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
